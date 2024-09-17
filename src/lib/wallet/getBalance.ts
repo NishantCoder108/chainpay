62d6e7dc6145208e5baa075b094cc897bb2f7020
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-09-17 20:47:08 +0530

    Make Get Sol Balance Context provide

--- src/lib/wallet/getBalance.ts
+++ src/lib/wallet/getBalance.ts
@@ -1,30 +1,23 @@
 import { Connection, PublicKey } from "@solana/web3.js";
 
-let cachedWallet: PublicKey | null = null;
-let cachedBal: number | null = null;
-
 const endpoint = process.env.NEXT_PUBLIC_RPC_URL as string;
 
 export async function getBalance(publicKey: PublicKey) {
     try {
-        if (publicKey === cachedWallet && cachedBal !== null) {
-            return cachedBal;
-        }
         const connection = new Connection(endpoint);
 
-        // const publicKey = new PublicKey(walletAddress);
-
-        const balanceLamports = await connection.getBalance(publicKey);
+        const balanceLamports = await connection.getBalance(
+            publicKey,
+            "confirmed"
+        );
 
         // Convert Lamports to SOL (1 SOL = 1 billion Lamports)
         const balanceSOL = balanceLamports / 1e9;
 
-        cachedWallet = publicKey;
-        cachedBal = balanceSOL;
-
         return balanceSOL;
     } catch (error) {
         console.error("Error fetching balance:", error);
-        throw error;
+
+        return 0;
     }
 }
